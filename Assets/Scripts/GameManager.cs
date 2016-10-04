e11f1bbd4d8a8d679e5fc88e021131605e3218fd
Ed Sibley <epsibley@gmail.com>
2016-10-04 14:51:35 +0100

    Next step in tutorials

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+	public bool recording = true;
+	private float fixedDeltaTime;
+	private bool isPaused = false;
+
+	void Start () {
+		fixedDeltaTime = Time.fixedDeltaTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1")) {
+			recording = false;
+		} else {
+			recording = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.P) && isPaused) {
+			ResumeGame();
+		} else if (Input.GetKeyDown (KeyCode.P) && !isPaused) {
+			PauseGame();
+		}
+	}
+
+	void PauseGame () {
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+		isPaused = true;
+	}
+	void ResumeGame () {
+		Time.timeScale = 1;
+		Time.fixedDeltaTime = fixedDeltaTime;
+		isPaused = false;
+	}
+
+	void OnApplicationPause (bool pauseStatus) {
+		isPaused = pauseStatus;
+	}
+}
---
+++ Assets/Scripts/ReplaySystem.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+	private Rigidbody rBody;
+	private GameManager gMan;
+
+	// Use this for initialization
+	void Start () {
+		rBody = GetComponent<Rigidbody> ();
+		gMan = FindObjectOfType<GameManager> ();
+	}
+	
+	void Update () {
+		if (gMan.recording == true) {
+			Record();
+		} else {
+			PlayBack();
+		}
+	}
+
+
+	public void PlayBack () {
+		rBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+
+	public void Record ()
+	{
+		rBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+
+/// <summary>
+///  A structure for storing time, position and rotation.
+/// </summary>
+public struct MyKeyFrame{
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float time, Vector3 pos, Quaternion rot) {
+		frameTime = time;
+		position = pos;
+		rotation = rot;
+	}
+}
